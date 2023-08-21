d684b4fb5901e017d97a84445a5cb0902a1120c4
Venkatesh nellore <51104553+samflynn007@users.noreply.github.com>
2023-08-21 17:01:34 +0530

    initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+## End to End ML Project
+
+
+### create a environment
+
+```
+conda create -p venv python==3.8
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)-> List[str]:
+    requirements = []
+    with open(file_path) as file_object:
+        requirements = file_object.readlines()
+        requirements=[req.replace('\n' ,"")for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+
+setup (
+    name = 'RegressorProject',
+    version='0.0.1',
+    author='Venkatesh',
+    author_email= 'nellorevenky007@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages= find_packages()
+)
