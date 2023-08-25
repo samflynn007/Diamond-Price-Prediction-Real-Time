6e0a18387109306f77a92276b7109de0a9b5f8ab
Venkatesh nellore <51104553+samflynn007@users.noreply.github.com>
2023-08-25 11:10:54 +0530

    added app.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT = '-e .'
+
 def get_requirements(file_path:str)-> List[str]:
     requirements = []
     with open(file_path) as file_object:
